8528b95088a8b377dd9db40028a9fdbd9290dfbe
Thales <thalesbarbosa.barbosa10@gmail.com>
2025-09-08 16:42:30 -0300

    alteração código único 2

---
+++ rh/migrations/0017_alter_codigo_fields.py
@@ -0,0 +1,20 @@
+from django.db import migrations, models
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ("rh", "0016"),  # última migration aplicada
+    ]
+
+    operations = [
+        migrations.AlterField(
+            model_name="desligamento",
+            name="codigo",
+            field=models.CharField("Código", max_length=20, db_index=True),
+        ),
+        migrations.AlterField(
+            model_name="admissao",
+            name="codigo",
+            field=models.CharField("Código RCA", max_length=20, db_index=True),
+        ),
+    ]
